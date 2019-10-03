89098581604f6b26088f492ba2b37a7ad4fb8727
kevin.xu <kevin.xu@imaginato.com>
2019-10-03 21:24:02 +0800

    a

--- app/src/main/java/com/mvvm/demo/MainActivity.kt
+++ app/src/main/java/com/mvvm/demo/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.mvvm.demo
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.mvvm.demo.view.common.base.BaseActivity
+import kotlinx.android.synthetic.main.default_app_bar.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun getContentResource() = R.layout.activity_main
+
+    override fun initViews() {
+        super.initViews()
     }
+
+    override fun getActionToolbar() = toolbar
+
+    override fun configureActionBar(actionBar: ActionBar) {
+        super.configureActionBar(actionBar)
+        with(actionBar) {
+            setHomeButtonEnabled(true)
+            setDisplayHomeAsUpEnabled(true)
+            title = "Main Screen"
+        }
+    }
+
 }
---
+++ app/src/main/java/com/mvvm/demo/view/common/base/BaseActivity.kt
@@ -0,0 +1,30 @@
+package com.mvvm.demo.view.common.base
+
+import android.os.Bundle
+import androidx.annotation.CallSuper
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getContentResource())
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+        setSupportActionBar(getActionToolbar())
+        supportActionBar?.let(this::configureActionBar)
+        initViews()
+    }
+
+    abstract fun getContentResource(): Int
+
+    @CallSuper
+    protected open fun initViews() = Unit
+
+    protected open fun getActionToolbar(): Toolbar? = null
+
+    protected open fun configureActionBar(actionBar: ActionBar) = Unit
+
+}
